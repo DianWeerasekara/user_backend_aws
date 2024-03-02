97717dfdf4627145bb79295e1cb6e0d0bfd0b2f5
DianWeerasekara <dianskyline@gmail.com>
2024-03-02 19:44:18 +0200

    create app.js
    server and the database was hosted and the router is also in the same file

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000; 
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'aws_service'
+});
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL Connected')
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+//create user
+app.post('/user', (req,res) => {
+    const { name, description } = req.body;
+    const newItem = { name, description }; 
+    const sql = 'INSERT INTO user_tbl SET ?';
+
+    db.query(sql, newItem, (err, result) => {
+        if(err){
+            res.status(500).send(err);
+        } else{
+            res.status(201).send('User registered successfully')
+        }
+    })
+})
+
+app.put('/user/:id', (req,res) => {
+    const { name, description } = req.body;
+    const updatedUser = { name, description }; 
+    const sql = 'UPDATE user_tbl SET ? WHERE id = ?';
+    const id = req.params.id;
+
+    db.query(sql, [updatedUser, id], (err, result) => {
+        if(err){
+            res.status(500).send(err);
+        } else {
+            res.status(200).send("User updated!")
+        }
+    })
+})
+
+// Read All Items
+app.get('/user', (req, res) => {
+    const sql = 'SELECT * FROM user_tbl';
+  
+    db.query(sql, (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).json(results);
+      }
+    });
+  });
+
+// Delete Item
+app.delete('/user/:id', (req, res) => {
+    const sql = 'DELETE FROM user_tbl WHERE id = ?';
+    const id = req.params.id;
+  
+    db.query(sql, id, (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send('Item deleted successfully');
+      }
+    });
+});
+
+module.exports = app;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
